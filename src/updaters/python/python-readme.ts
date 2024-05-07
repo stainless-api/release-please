b33e7761ba5fbadf98acf3ca2be10707baf9c28b
meorphis <meorphis@gmail.com>
2024-05-07 17:01:57 -0400

    fix regex for windows

--- src/updaters/python/python-readme.ts
+++ src/updaters/python/python-readme.ts
@@ -10,17 +10,20 @@ export class PythonReadme extends DefaultUpdater {
    * @returns {string} The updated content
    */
   updateContent(content: string): string {
+    // differs for windows vs. linux/ios
+    const newlineType = content.includes('\r\n') ? '\r\n' : '\n';
+
     if (this.version.preRelease) {
       // it's a pre-release, so add the pre-release flag if it's missing
       return content.replace(
-        /^# install from PyPI\n^pip install (\S+)$/gm,
-        '# install from PyPI\npip install --pre $1'
+        /^# install from PyPI\r?\n^pip install (\S+)$/gm,
+        `# install from PyPI${newlineType}pip install --pre $1`
       );
     } else {
       // it's not a pre-release, so remove the pre-release flag if it's present
       return content.replace(
-        /^# install from PyPI\n^pip install --pre (\S+)$/gm,
-        '# install from PyPI\npip install $1'
+        /^# install from PyPI\r?\n^pip install --pre (\S+)$/gm,
+        `# install from PyPI${newlineType}pip install $1`
       );
     }
   }
